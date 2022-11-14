eb05164b73b0ae1b44e35f307c435dce221fc544
William Miranda <william.miranda@utp.ac.pa>
2022-11-13 21:02:33 -0500

    Creacion de repositorio para Parcial #1.

--- lab07/class/miclase.php
+++
@@ -1,9 +0,0 @@
-<?php
-class miclase
-{
-    public function __construct()
-    {
-        echo "Mi Clase ha sido agregada!!! <br>";
-    }
-}
-?>
--- lab07/class/miotraclase.php
+++
@@ -1,7 +0,0 @@
-<?php
-class miotraclase {
-    public function __construct()
-    {
-        echo "Mi segunda Clase ha sido agregada!!! <br>";
-    }
-}
